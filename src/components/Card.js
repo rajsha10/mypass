b7ac3eaef413c46b989e7b73cce8417af43d3a99
rajsha10 <rajsha8617@gmail.com>
2025-01-05 21:43:07 +0530

    stylings and testing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import { ethers } from 'ethers';
+import '../css/Card.css';
 
 const Card = ({ occasion, toggle, setToggle, setOccasion, src }) => {
   const togglePop = () => {
@@ -7,27 +8,44 @@ const Card = ({ occasion, toggle, setToggle, setOccasion, src }) => {
   };
 
   return (
-    <div className="card">
-      <img src={src} alt={occasion.name} className="card__thumbnail" />
-      <div className="card__info">
-        <p className="card__date">
-          <strong>{occasion.date}</strong>
-          <br />
-          {occasion.time}
-        </p>
-        <h3 className="card__name">{occasion.name}</h3>
-        <p className="card__location">
-          <small>{occasion.location}</small>
-        </p>
-        <p className="card__cost">
-          <strong>{ethers.utils.formatUnits(occasion.cost.toString(), 'ether')} $EDU</strong>
-        </p>
+    <div className="event-card">
+      <div className="event-card__image-container">
+        <img src={src} alt={occasion.name} className="event-card__image" />
+      </div>
+      
+      <div className="event-card__content">
+        <div className="event-card__datetime">
+          <span className="event-card__date">{occasion.date}</span>
+          <span className="event-card__time">{occasion.time}</span>
+        </div>
+
+        <h3 className="event-card__title">{occasion.name}</h3>
+        
+        <div className="event-card__details">
+          <div className="event-card__location">
+            <i className="location-icon">📍</i>
+            <span>{occasion.location}</span>
+          </div>
+          
+          <div className="event-card__price">
+            {ethers.utils.formatUnits(occasion.cost.toString(), 'ether')} $EDU
+          </div>
+        </div>
+
         {occasion.tickets.toString() === "0" ? (
-          <button type="button" className="card__button--out" disabled>
+          <button 
+            type="button" 
+            className="event-card__button event-card__button--sold-out" 
+            disabled
+          >
             Sold Out
           </button>
         ) : (
-          <button type="button" className="card__button" onClick={() => togglePop()}>
+          <button 
+            type="button" 
+            className="event-card__button" 
+            onClick={togglePop}
+          >
             View Seats
           </button>
         )}
